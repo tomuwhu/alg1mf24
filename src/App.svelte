f21d0b8284777634923ec6cbf49331786e395d3c
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-30 15:25:29 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,23 @@
     b: "Rekurzzióval megoldható feladatok",
     c: "Dinamikus programozással megoldható feladat",
     d: "Fa- és gráfalgoritmusos feladatok",
-    e: "SPOJ/OKTV"
-  }
+    e: "SPOJ/OKTV",
+  };
   const h = {
-    "Urbán Ákos": "https://github.com/akosh25/algoritmusok"
-  }
+    "Urbán Ákos": "https://github.com/akosh25/algoritmusok",
+  };
   const mf = {
     HR0Lily: {
       type: ft.a,
       fl: "https://www.hackerrank.com/challenges/lilys-homework/problem?isFullScreen=true",
-      mo: ["Urbán Ákos"]
+      mo: ["Urbán Ákos"],
     },
     HR0Stone_Division: {
       type: ft.b,
       fl: "https://www.hackerrank.com/challenges/lilys-homework/problem?isFullScreen=true",
-      mo: ["Urbán Ákos"]
-    }
-  }
+      mo: ["Urbán Ákos"],
+    },
+  };
 </script>
 
 <main>
@@ -30,7 +30,8 @@
     {#each Object.entries(mf) as f}
       <tr>
         <th>
-          <a target="_blank" href={f[1].fl}>{f[0]}</a>:
+          <a target="_blank" href={f[1].fl}>{f[0]}</a> 
+          <span>({mf[f[0]].type})</span>:
         </th>
         <td>
           {#each f[1].mo as hallg}
@@ -43,6 +44,10 @@
 </main>
 
 <style>
+  span {
+    font-size: 10px;
+    color:gray;
+  }
   h1 {
     font-size: 20px;
   }
