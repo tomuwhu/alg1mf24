d6adc02e1974ad3c2275de4420c358f75da9b04c
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-06 19:46:55 +0200

    .

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,7 @@
   };
   const h = {
     "Urbán Ákos": "https://github.com/akosh25/algoritmusok",
+    "Sámfira Gábor": "https://github.com/Infinity42o/L-Algoritmusok-s-adatszerkezetek"
   };
   const mf = {
     HR0Lily: {
@@ -21,11 +22,31 @@
       fl: "https://www.hackerrank.com/challenges/lilys-homework/problem?isFullScreen=true",
       mo: ["Urbán Ákos"],
     },
+    Gridland_Metro: {
+      type: ft.a,
+      fl: "https://www.hackerrank.com/challenges/gridland-metro/problem",
+      mo: ["Sámfira Gábor"],
+    },
+    Crossword_Puzzle: {
+      type: ft.b,
+      fl: "https://www.hackerrank.com/challenges/crossword-puzzle/problem",
+      mo: ["Sámfira Gábor"],
+    },
+    Sherlock_and_Cost: {
+      type: ft.c,
+      fl: "https://www.hackerrank.com/challenges/sherlock-and-cost/problem",
+      mo: ["Sámfira Gábor"],
+    },
+    Even_Tree: {
+      type: ft.d,
+      fl: "https://www.hackerrank.com/challenges/even-tree/problem",
+      mo: ["Sámfira Gábor"],
+    },
   };
 </script>
 
 <main>
-  <h1>Leadott feladatok</h1>
+  <h1>Leadott (elkelt) feladatok</h1>
   <table>
     {#each Object.entries(mf) as f}
       <tr>
